ee3fb06fda6ae2f7c61241bcd41a905a35cb0e54
jmh491 <43830827+jmh491@users.noreply.github.com>
2018-10-03 22:42:47 -0400

    Added RTS style Camera w/ controls
    WASD controlled camera and some minor tweaks to navmesh agents. Will probably need to refine camera controls further going ahead, but they are pretty good for now.

---
+++ Monkeys the Game/Assets/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float panSpeed = 20f;
+    public Vector2 panLimitX; //how far camera can pan horizontally before it clamps
+    public Vector2 panLimitY; //how far camera can pan vertically before it clamps
+
+    public float zoomSpeed = 20f;
+    public float zoomLimitMin = 20f; //height in Y for how far in we can zoom before clamp
+    public float zoomLimitMax = 120f; //height in Y for how far out we can zoom
+
+    // Update is called once per frame
+    void Update () {
+
+        Vector3 pos = transform.position;
+        //this controls camera panning with wasd
+        if (Input.GetKey("w")){
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        //this controls the zoom
+        float zoom = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= zoom * zoomSpeed * 20f * Time.deltaTime;
+
+        //this controls panning and zooming limits
+        pos.x = Mathf.Clamp(pos.x, panLimitX.x, panLimitX.y);
+        pos.y = Mathf.Clamp(pos.y, zoomLimitMin, zoomLimitMax);
+        pos.z = Mathf.Clamp(pos.z, panLimitY.x, panLimitY.y);//use y because we used vector2, so this y is actually our z
+
+        transform.position = pos;
+	}
+}
