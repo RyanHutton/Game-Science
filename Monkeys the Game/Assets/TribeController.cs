de32361b201f195161640fa1e12718c2c0705573
jmh491 <43830827+jmh491@users.noreply.github.com>
2018-10-04 23:26:46 -0400

    Added Hut
    Added basic Hut with very simplistic spawn feature that spawns monkeys in front of itself when space is held down.

---
+++ Monkeys the Game/Assets/TribeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TribeController : MonoBehaviour {
+    public int bananas = 0;
+    public int population;
+    public int numOfGatherers;
+    public int numOfSoldiers;
+    public GameObject monkey;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (numOfGatherers + numOfSoldiers < population)
+        {
+            AllocateUnits();
+        }
+
+        if (Input.GetKey("space")) //implement cycles, use timers?
+        {
+            SpawnUnits();
+        }
+	}
+
+    void AllocateUnits()
+    {
+        while (numOfGatherers + numOfSoldiers < population)
+        {
+            //temp hardcoded assignments
+
+        }
+    }
+
+    void SpawnUnits()
+    {
+        Vector3 spawnPoint = transform.position; //grabs position of hut
+        spawnPoint.x += Random.Range(5f, 15f); //sets spawn of monkeys to be in a random range in front of the hut
+        spawnPoint.z += Random.Range(-5f, 5f);
+        GameObject unit = (GameObject)Instantiate(monkey, spawnPoint, transform.rotation);
+        population++;
+
+    }
+}
