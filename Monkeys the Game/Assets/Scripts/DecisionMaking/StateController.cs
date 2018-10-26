794b96b6fff86e736d16d86bed6604ab62bfb6b9
Ryan <ryanhutton07@gmail.com>
2018-10-25 21:15:12 -0400

    Fixed Waypoint System for Patrol
    Player (and subsequently tribe) will default to patrolling and searching for bananas while not in conflict or being given commands from Human player.

--- Monkeys the Game/Assets/Scripts/DecisionMaking/StateController.cs
+++ Monkeys the Game/Assets/Scripts/DecisionMaking/StateController.cs
@@ -11,9 +11,10 @@ public class StateController : MonoBehaviour
     public EnemyStats enemyStats;
     public Transform eyes;
     public State remainState;
+    public List<Transform> wayPoints;
 
 
-    [HideInInspector] public NavMeshAgent navMeshAgent;
+     public NavMeshAgent navMeshAgent;
     [HideInInspector] public List<Transform> wayPointList;
     [HideInInspector] public int nextWayPoint;
     [HideInInspector] public Transform chaseTarget;
@@ -24,26 +25,32 @@ public class StateController : MonoBehaviour
 
     void Awake()
     {
-        //tankShooting = GetComponent<Complete.TankShooting>();
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        Debug.Log("Got here");
+        wayPointList = wayPoints;
+        navMeshAgent.enabled = true;
+        navMeshAgent = GetComponent<NavMeshAgent> ();
+        navMeshAgent.enabled = true;
+
     }
 
-    public void SetupAI(bool aiActivationFromTankManager, List<Transform> wayPointsFromTankManager)
+    public void SetupAI(bool aiActivationFromManager, List<Transform> wayPointsFromManager)
     {
-        wayPointList = wayPointsFromTankManager;
-        aiActive = aiActivationFromTankManager;
-        if (aiActive)
-        {
-            navMeshAgent.enabled = true;
-        }
-        else
-        {
-            navMeshAgent.enabled = false;
-        }
+        //Debug.Log("Got here?");
+        //wayPointList = wayPointsFromManager;
+        //aiActive = aiActivationFromManager;
+        //if (aiActive)
+        //{
+        //    navMeshAgent.enabled = true;
+        //}
+        //else
+        //{
+        //    navMeshAgent.enabled = false;
+        //}
     }
 
     void Update()
     {
+        aiActive = true;
         if (!aiActive)
             return;
         currentState.UpdateState(this);
